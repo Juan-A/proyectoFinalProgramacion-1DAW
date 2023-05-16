718dc887dcb6e5596bcb43ef7f974d81504bdadc
Juan Antonio Herrero <juan13herrero@gmail.com>
2023-05-16 10:34:13 +0200

    Eliminado import y vars. redundantes.

--- src/partida/tiposPartida/JugadorVsJugador.java
+++ src/partida/tiposPartida/JugadorVsJugador.java
@@ -3,13 +3,10 @@ package partida.tiposPartida;
 import jugadores.Persona;
 import partida.Partida;
 
-import java.util.Random;
 
 public class JugadorVsJugador extends Partida {
     public JugadorVsJugador() {
         super();
-        boolean algunoPuedeMover = true, puedeMoverJugador1 = true, puedeMoverJugador2 = true;
-
 
         jugadores[0] = new Persona();
         jugadores[0].iniciarPartida(true);
@@ -19,7 +16,6 @@ public class JugadorVsJugador extends Partida {
         imprimirColoresJugadores();
 
         jugadas();
-
 
 
     }
