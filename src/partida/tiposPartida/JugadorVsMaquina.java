b8eefa8bfd9fa4334f515ab75db688462a1aab00
Juan Antonio Herrero <juan13herrero@gmail.com>
2023-05-19 21:43:10 +0200

    Arreglado fallo menor.

--- src/partida/tiposPartida/JugadorVsMaquina.java
+++ src/partida/tiposPartida/JugadorVsMaquina.java
@@ -21,6 +21,7 @@ public class JugadorVsMaquina extends Partida {
         jugadores[primerJugador] = new Persona();
         jugadores[primerJugador].iniciarPartida(true);
         jugadores[segundoJugador] = new Maquina();
+        jugadores[segundoJugador].iniciarPartida(false);
         jugadores[segundoJugador].setColor(jugadores[0].colorContrarioAdversario());
         imprimirColoresJugadores();
 
